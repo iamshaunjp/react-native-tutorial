e7a4b8a2cb683c44cd7ee49348fa76ec215be2c9
Shaun <shaun@thenetninja.co.uk>
2019-10-22 18:30:27 +0100

    lesson-4

--- my-project/App.js
+++ my-project/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('shaun');
+  const [person, setPerson] = useState({ name: 'mario', age: 40 });
+
+  const clickHandler = () => {
+    setName('chun-li');
+    setPerson({ name: 'luigi', age: 45 });
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World!</Text>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler} />  
       </View>
-      <View style={styles.body}>
-        <Text>Lorem ipsum <Text style={styles.boldText}>dolor</Text> sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-      </View>
-      
     </View>
   );
 }
@@ -24,15 +28,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: 'bold',
+  buttonContainer: {
+    marginTop: 20,
   }
 });
