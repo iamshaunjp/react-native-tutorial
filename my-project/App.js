f16fefcd236c4b931049f4c76ddd3734b820a062
Shaun <shaun@thenetninja.co.uk>
2019-10-23 11:56:16 +0100

    lesson-10

--- my-project/App.js
+++ my-project/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -9,6 +10,11 @@ export default function App() {
     { text: 'play on the switch', key: '3' }
   ]);
 
+  const pressHandler = (key) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  };
 
   return (
     <View style={styles.container}>
@@ -19,7 +25,7 @@ export default function App() {
           <FlatList
             data={todos}
             renderItem={({ item }) => (
-              <Text>{item.text}</Text>
+              <TodoItem item={item} pressHandler={pressHandler} />
             )}
           />
         </View>
