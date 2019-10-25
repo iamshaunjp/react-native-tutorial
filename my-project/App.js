ed3b35e7d840376679fd04a3dfda59de49f8307d
Shaun <shaun@thenetninja.co.uk>
2019-10-25 12:04:26 +0100

    lesson-13

--- my-project/App.js
+++ my-project/App.js
@@ -19,7 +19,6 @@ export default function App() {
 
   const submitHandler = (text) => {
     if(text.length > 3){
-      setText('');
       setTodos(prevTodos => {
         return [
           { text, key: Math.random().toString() },
