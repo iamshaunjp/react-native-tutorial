2ec466473e52b94b17478525b1e3fde2ab4f1b7e
Shaun <shaun@thenetninja.co.uk>
2019-10-26 17:42:53 +0100

    lesson-27

--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetails({ navigation }) {
+  const rating = navigation.getParam('rating');
+  
   return (
     <View style={globalStyles.container}>
       <Card>
@@ -11,8 +13,22 @@ export default function ReviewDetails({ navigation }) {
           { navigation.getParam('title') }
         </Text>
         <Text>{ navigation.getParam('body') }</Text>
-        <Text>{ navigation.getParam('rating') }</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  }
+});
--- gamezone/styles/global.js
+++ gamezone/styles/global.js
@@ -15,3 +15,13 @@ export const globalStyles = StyleSheet.create({
     padding: 20,
   },
 });
+
+export const images = {
+  ratings: {
+    '1': require('../assets/rating-1.png'),
+    '2': require('../assets/rating-2.png'),
+    '3': require('../assets/rating-3.png'),
+    '4': require('../assets/rating-4.png'),
+    '5': require('../assets/rating-5.png'),
+  }
+};
