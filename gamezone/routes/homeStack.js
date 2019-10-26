40e4668347c4a8bf4800b53587fd19f0008cca65
Shaun <shaun@thenetninja.co.uk>
2019-10-26 10:40:18 +0100

    lesson-24

--- gamezone/routes/homeStack.js
+++ gamezone/routes/homeStack.js
@@ -6,14 +6,27 @@ import ReviewDetails from '../screens/reviewDetails';
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: 'GameZone',
+      //headerStyle: { backgroundColor: '#eee' }
+    }
   },
   ReviewDetails: {
     screen: ReviewDetails,
+    navigationOptions: {
+      title: 'Review Details',
+      //headerStyle: { backgroundColor: '#eee' }
+    }
   },
 };
 
 // home stack navigator screens
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: '#444',
+    headerStyle: { backgroundColor: '#eee', height: 60 }
+  }
+});
 
 export default createAppContainer(HomeStack);
 
