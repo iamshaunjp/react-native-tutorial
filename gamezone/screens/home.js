2ab4cf5962c85f5f80ef714f94ebe734c392952c
Shaun <shaun@thenetninja.co.uk>
2019-10-26 09:28:28 +0100

    lesson-22

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function Home({ navigation }) {
-
-  const pressHandler = () => {
-    //navigation.navigate('ReviewDetails');
-    navigation.push('ReviewDetails');
-  }
+  const [reviews, setReviews] = useState([
+    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+    { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+    { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+  ]);
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Button title='to review details screen' onPress={pressHandler} />
+      <FlatList data={reviews} renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+          <Text style={globalStyles.titleText}>{ item.title }</Text>
+        </TouchableOpacity>
+      )} />
     </View>
   );
 }
--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function ReviewDetails({ navigation }) {
-
-  const pressHandler = () => {
-    navigation.goBack();
-  }
-
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails Screen</Text>
-      <Button title='back to home screen' onPress={pressHandler} />
+      <Text>{ navigation.getParam('title') }</Text>
+      <Text>{ navigation.getParam('body') }</Text>
+      <Text>{ navigation.getParam('rating') }</Text>
     </View>
   );
 }
