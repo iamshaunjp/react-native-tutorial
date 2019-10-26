54f44f957abd01951459d0e95330b4aa94b48401
Shaun <shaun@thenetninja.co.uk>
2019-10-26 11:40:33 +0100

    lesson-26

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { globalStyles } from '../styles/global';
+import Card from '../shared/card';
 
 export default function Home({ navigation }) {
   const [reviews, setReviews] = useState([
@@ -13,7 +14,9 @@ export default function Home({ navigation }) {
     <View style={globalStyles.container}>
       <FlatList data={reviews} renderItem={({ item }) => (
         <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
-          <Text style={globalStyles.titleText}>{ item.title }</Text>
+          <Card>
+            <Text style={globalStyles.titleText}>{ item.title }</Text>
+          </Card>
         </TouchableOpacity>
       )} />
     </View>
--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
+import Card from '../shared/card';
 
 export default function ReviewDetails({ navigation }) {
   return (
     <View style={globalStyles.container}>
-      <Text>{ navigation.getParam('title') }</Text>
-      <Text>{ navigation.getParam('body') }</Text>
-      <Text>{ navigation.getParam('rating') }</Text>
+      <Card>
+        <Text style={globalStyles.titleText}>
+          { navigation.getParam('title') }
+        </Text>
+        <Text>{ navigation.getParam('body') }</Text>
+        <Text>{ navigation.getParam('rating') }</Text>
+      </Card>
     </View>
   );
 }
