09ec94861bee517df4264fd8383924de086f92cb
Shaun <shaun@thenetninja.co.uk>
2019-10-26 17:55:47 +0100

    lesson-28

--- gamezone/shared/header.js
+++ gamezone/shared/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header({ title, navigation }) {
@@ -9,12 +9,13 @@ export default function Header({ title, navigation }) {
   }
 
   return (
-    <View style={styles.header}>
+    <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
       <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
-      <View>
+      <View style={styles.headerTitle}>
+        <Image source={require('../assets/heart_logo.png')} style={styles.headerImage} />
         <Text style={styles.headerText}>{title}</Text>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
@@ -35,5 +36,13 @@ const styles = StyleSheet.create({
   icon: {
     position: 'absolute',
     left: 16,
-  }
+  },
+  headerTitle: {
+    flexDirection: 'row'
+  },
+  headerImage: {
+    width: 26,
+    height: 26,
+    marginHorizontal: 10
+  },
 });
