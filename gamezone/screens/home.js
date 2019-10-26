09c0923e0eebd7d58ebee1b179c5060c0fe17c28
Shaun <shaun@thenetninja.co.uk>
2019-10-26 18:40:02 +0100

    lesson-29

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, FlatList, Modal } from 'react-native';
 import { globalStyles } from '../styles/global';
+import { MaterialIcons } from '@expo/vector-icons';
 import Card from '../shared/card';
 
 export default function Home({ navigation }) {
+  const [modalOpen, setModalOpen] = useState(false);
   const [reviews, setReviews] = useState([
     { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
     { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
@@ -12,6 +14,26 @@ export default function Home({ navigation }) {
 
   return (
     <View style={globalStyles.container}>
+  
+      <Modal visible={modalOpen} animationType='slide'>
+        <View style={styles.modalContent}>
+          <MaterialIcons 
+            name='close'
+            size={24} 
+            style={{...styles.modalToggle, ...styles.modalClose}} 
+            onPress={() => setModalOpen(false)} 
+          />
+          <Text>Hello from the modal :)</Text>
+        </View>
+      </Modal>
+
+      <MaterialIcons 
+        name='add' 
+        size={24} 
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)} 
+      />
+
       <FlatList data={reviews} renderItem={({ item }) => (
         <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
           <Card>
@@ -19,6 +41,27 @@ export default function Home({ navigation }) {
           </Card>
         </TouchableOpacity>
       )} />
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modalToggle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  },
+  modalContent: {
+    flex: 1,
+  }
+});
