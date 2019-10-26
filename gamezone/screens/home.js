2cc3d318234d9113c3b45cd1c99719b330164b28
Shaun <shaun@thenetninja.co.uk>
2019-10-26 18:58:33 +0100

    lesson-30

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text, TouchableOpacity, FlatList, Modal } from 'react
 import { globalStyles } from '../styles/global';
 import { MaterialIcons } from '@expo/vector-icons';
 import Card from '../shared/card';
+import ReviewForm from './reviewForm';
 
 export default function Home({ navigation }) {
   const [modalOpen, setModalOpen] = useState(false);
@@ -23,7 +24,7 @@ export default function Home({ navigation }) {
             style={{...styles.modalToggle, ...styles.modalClose}} 
             onPress={() => setModalOpen(false)} 
           />
-          <Text>Hello from the modal :)</Text>
+          <ReviewForm />
         </View>
       </Modal>
 
--- gamezone/styles/global.js
+++ gamezone/styles/global.js
@@ -14,6 +14,13 @@ export const globalStyles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 6,
+  },
 });
 
 export const images = {
