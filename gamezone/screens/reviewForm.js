455bcca3b0753f50e00cef70b920d360c55a518d
Shaun <shaun@thenetninja.co.uk>
2019-10-27 09:45:15 +0000

    lesson-32

--- gamezone/screens/reviewForm.js
+++ gamezone/screens/reviewForm.js
@@ -2,6 +2,21 @@ import React from 'react';
 import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global.js';
 import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const reviewSchema = yup.object({
+  title: yup.string()
+    .required()
+    .min(4),
+  body: yup.string()
+    .required()
+    .min(8),
+  rating: yup.string()
+    .required()
+    .test('is-num-1-5', 'Rating must be a number 1 - 5', (val) => {
+      return parseInt(val) < 6 && parseInt(val) > 0;
+    }),
+});
 
 export default function ReviewForm({ addReview }) {
 
@@ -10,6 +25,7 @@ export default function ReviewForm({ addReview }) {
     <View style={globalStyles.container}>
       <Formik
         initialValues={{ title: '', body: '', rating: '' }}
+        validationSchema={reviewSchema}
         onSubmit={(values, actions) => {
           actions.resetForm(); 
           addReview(values);
